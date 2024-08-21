9880dde880c79a79792a417a2a1bb08ff31cf53a
nate-slowey-BU <nslowey@my365.bellevue.edu>
2024-08-20 20:27:02 -0500

    week 2 chapter complete

---
+++ week-2/chapter/js02_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Nate Slowey
+      Date: 8/19
+
+      Filename: js02.js
+ */
+
