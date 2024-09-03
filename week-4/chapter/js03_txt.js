cdba368038a0ecc8c703318e3e940401259d5fc1
nate-slowey-BU <nslowey@my365.bellevue.edu>
2024-09-03 18:35:15 -0500

    week 4 chapter complete

---
+++ week-4/chapter/js03_txt.js
@@ -0,0 +1,15 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Nate Slowey
+     Date:   9/2/24
+
+     Filename: js03.js
+ */
+
+// Days of the week 
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
