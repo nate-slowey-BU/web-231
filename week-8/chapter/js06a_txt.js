1a4c389a1a3287794260dc4fa29adc7d1150b0c1
nate-slowey-BU <nslowey@my365.bellevue.edu>
2024-10-02 17:35:09 -0500

    week 8 chapter complete

---
+++ week-8/chapter/js06a_txt.js
@@ -0,0 +1,14 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Chapter case
+
+      Order Form Code
+      Author: Nate Slowey
+      Date:   9/30
+
+      Filename: js06a.js
+ */
+
+
+
