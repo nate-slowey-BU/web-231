63440038c7949f842e82567007afa591247cafc9
nate-slowey-BU <nslowey@my365.bellevue.edu>
2024-10-10 18:28:19 -0500

    week 9 project complete

---
+++ week-9/project07-01/project07-01.js
@@ -0,0 +1,35 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 7
+      Project 07-01
+
+      Project to validate a form used for setting up a new account
+      Author: Nate Slowey
+      Date: 10/10
+
+      Filename: project07-01.js
+*/
+
+let signupForm = document.getElementById("signup");
+
+signupForm.addEventListener("submit", function (e) {
+  let pwd = document.getElementById("pwd").value;
+  let feedback = document.getElementById("feedback");
+  e.preventDefault();
+  let regex1 = /[A-Z]/;
+  let regex2 = /\d/;
+  let regex3 = /[!#%\$]/;
+
+  if (pwd.length < 8) {
+    feedback.textContent = "Your password must be at least 8 characters.";
+  } else if (!regex1.test(pwd)) {
+    feedback.textContent = "Your password must include an uppercase letter.";
+  } else if (!regex2.test(pwd)) {
+    feedback.textContent = "Your password must contain a number.";
+  } else if (!regex3.test(pwd)) {
+    feedback.textContent =
+      "Your password must include one of the following: !$#%";
+  } else {
+    signupForm.submit();
+  }
+});
